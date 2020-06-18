9c11d262b49a8790819c31f6ffc4af94d6cf7f00
Eric Maino <eric.maino@microsoft.com>
2020-06-17 23:09:14 -0700

    REFACTOR: Extract IsUserLoggedIn into it's own decorator

---
+++ src/AuthJanitor.Functions.AdminApi/Services/ProvidersServiceActionResultAdapter.cs
@@ -0,0 +1,71 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+using AuthJanitor.UI.Shared;
+using AuthJanitor.UI.Shared.MetaServices;
+using AuthJanitor.UI.Shared.ViewModels;
+using AuthJanitor.EventSinks;
+using AuthJanitor.IdentityServices;
+using AuthJanitor.Providers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace AuthJanitor.Services
+{
+    /// <summary>
+    /// API functions to describe the loaded Providers and their configurations.
+    /// A Provider is a library containing logic to either rekey an object/service or manage the lifecycle of an application.
+    /// </summary>
+    public class ProvidersServiceActionResultAdapter
+    {
+        private readonly IIdentityService _identityService;
+        private readonly ProvidersService _providers;
+
+        public ProvidersServiceActionResultAdapter(
+            IIdentityService identityService,
+            ProvidersService providers)
+        {
+            _identityService = identityService;
+            _providers = providers;
+        }
+
+        public async Task<IActionResult> List()
+        {
+            return await ValidateLogin(() => Task.FromResult(_providers.List()));
+        }
+
+        public async Task<IActionResult> GetBlankConfiguration(string providerType)
+        {
+            return await ValidateLogin(async () => await _providers.GetBlankConfiguration(providerType));
+        }
+
+        public async Task<IActionResult> TestConfiguration(
+            string providerConfiguration,
+            string providerType,
+            string testContext)
+        {
+            return await ValidateLogin(async () => await _providers.TestConfiguration(providerConfiguration, providerType, testContext));
+        }
+
+        private async Task<IActionResult> ValidateLogin(Func<Task<IActionResult>> func)
+        {
+            IActionResult result;
+
+            if (_identityService.IsUserLoggedIn)
+            {
+                result = await func();
+            }
+            else
+            {
+                result = new UnauthorizedResult();
+            }
+
+            return result;
+        }
+    }
+}
